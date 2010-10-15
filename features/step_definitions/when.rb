d0334e4972fae7fc2a4a770466e72666cecf739c
Klaus Kämpf <kkaempf@suse.de>
2010-10-15 12:46:19 +0200

    Use local genprovider implementation during testing

--- features/step_definitions/when.rb
+++ features/step_definitions/when.rb
@@ -1,15 +1,15 @@
 When /^I run genprovider with no arguments$/ do
-  @output = `ruby #{GENPROVIDER} 2> stderr.out`
+  @output = `ruby -I #{LIBDIR} #{GENPROVIDER} 2> stderr.out`
   raise if $? == 0
 end
 
 When /^I pass "([^"]*)" to genprovider$/ do |arg1| #"
-  @output = `ruby #{GENPROVIDER} -n test/test #{File.join(MOFDIR, arg1)} 2> stderr.out`
+  @output = `ruby -I #{LIBDIR} #{GENPROVIDER} -n test/test #{File.join(MOFDIR, arg1)} 2> stderr.out`
   raise unless $? == 0
 end
 
 When /^I run genprovider with "([^"]*)"$/ do |arg1| #"
-  @output = `ruby #{GENPROVIDER} #{arg1} 2> stderr.out`
+  @output = `ruby -I #{LIBDIR} #{GENPROVIDER} #{arg1} 2> stderr.out`
   raise unless $? == 0
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,17 @@
 require 'rubygems'
 require "test/unit"
 
+# establish parent for test data
 PARENT = File.expand_path(File.join(File.dirname(__FILE__),".."))
-TOPLEVEL = File.expand_path(File.join(PARENT,"..",".."))
-GENPROVIDER = File.join(TOPLEVEL,"genprovider.rb")
+
+# directory with test .mof files
 MOFDIR = File.join(PARENT,"mof")
+
+# toplevel directory
+TOPLEVEL = File.expand_path(File.join(PARENT,".."))
+
+# use local version for testing
+LIBDIR = File.join(TOPLEVEL, "lib")
+
+# genprovider cli binary
+GENPROVIDER = File.join(TOPLEVEL,"bin","genprovider")
