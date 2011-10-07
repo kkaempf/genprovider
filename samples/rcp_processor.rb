c134954461134825735fcde321763ddf5eb1a57f
Klaus Kämpf <kkaempf@suse.de>
2011-10-07 21:05:53 +0200

    Full property list in sample provider

--- samples/rcp_processor.rb
+++ samples/rcp_processor.rb
@@ -18,10 +18,64 @@ module Cmpi
     #
     def each( reference, properties = nil, want_instance = false )
       result = Cmpi::CMPIObjectPath.new reference
-      result[:system_creation_class_name] = "RCP_Processor" # string
-      result[:system_name] = "hostname" # string
-      result[:creation_class_name] = "RCP_Processor" # string
-      result[:device_id] = "0" # string
+      result[:system_creation_class_name] = nil # string
+      result[:system_name] = nil # string
+      result[:creation_class_name] = nil # string
+      result[:device_id] = nil # string
+      yield result unless want_instance
+      
+      # result[:instance_id] = nil # string
+      # result[:caption] = nil # string
+      # result[:description] = nil # string
+      # result[:element_name] = nil # string
+      # result[:generation] = nil # uint64
+      # result[:install_date] = nil # datetime
+      # result[:name] = nil # string
+      # result[:operational_status] = nil # uint16[]
+      # result[:status_descriptions] = nil # string[]
+      # result[:status] = nil # string
+      # result[:health_state] = nil # uint16
+      # result[:primary_status] = nil # uint16
+      # result[:detailed_status] = nil # uint16
+      # result[:operating_status] = nil # uint16
+      # result[:communication_status] = nil # uint16
+      # result[:enabled_state] = nil # uint16
+      # result[:other_enabled_state] = nil # string
+      # result[:requested_state] = nil # uint16
+      # result[:enabled_default] = nil # uint16
+      # result[:time_of_last_state_change] = nil # datetime
+      # result[:available_requested_states] = nil # uint16[]
+      # result[:transitioning_to_state] = nil # uint16
+      # result[:power_management_supported] = nil # bool
+      # result[:power_management_capabilities] = nil # uint16[]
+      # result[:availability] = nil # uint16
+      # result[:status_info] = nil # uint16
+      # result[:last_error_code] = nil # uint32
+      # result[:error_description] = nil # string
+      # result[:error_cleared] = nil # bool
+      # result[:other_identifying_info] = nil # string[]
+      # result[:power_on_hours] = nil # uint64
+      # result[:total_power_on_hours] = nil # uint64
+      # result[:identifying_descriptions] = nil # string[]
+      # result[:additional_availability] = nil # uint16[]
+      # result[:max_quiesce_time] = nil # uint64
+      # result[:location_indicator] = nil # uint16
+      # result[:role] = nil # string
+      # result[:family] = nil # uint16
+      # result[:other_family_description] = nil # string
+      # result[:upgrade_method] = nil # uint16
+      # result[:max_clock_speed] = nil # uint32
+      # result[:current_clock_speed] = nil # uint32
+      # result[:data_width] = nil # uint16
+      # result[:address_width] = nil # uint16
+      # result[:load_percentage] = nil # uint16
+      # result[:stepping] = nil # string
+      # result[:unique_id] = nil # string
+      # result[:cpu_status] = nil # uint16
+      # result[:external_bus_clock_speed] = nil # uint32
+      # result[:characteristics] = nil # uint16[]
+      # result[:number_of_enabled_cores] = nil # uint16
+      # result[:enabled_processor_characteristics] = nil # uint16[]
       yield result
     end
     public
