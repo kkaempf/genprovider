40d5bf581e64d72eff3bcb6bc1539b1977973934
Klaus Kämpf <kkaempf@suse.de>
2011-10-09 15:28:15 +0200

    Keep class and output as instance properties

--- lib/genprovider/provider.rb
+++ lib/genprovider/provider.rb
@@ -27,7 +27,8 @@ module Genprovider
     #  filter = :keys  # keys only
     #           :nokey # non-keys only
     #           :all   # all
-    def properties c, filter
+    def properties filter
+      c = @klass
       while c
 	c.features.each do |f|
 	  next unless f.property?
@@ -62,167 +63,167 @@ module Genprovider
     #
     # Class#each
     #
-    def mkeach c, out
-      out.puts "private"
-      out.comment
-      out.comment "Iterator for names and instances"
-      out.comment " yields references matching reference and properties"
-      out.comment
-      out.def "each", "reference", "properties = nil", "want_instance = false"
-      out.puts "result = Cmpi::CMPIObjectPath.new reference"
-      out.puts
-      out.comment "Set key properties"
-      out.puts
-      properties(c, :keys) do |prop|
-	out.puts(property_setter_line prop)
+    def mkeach
+      @out.puts "private"
+      @out.comment
+      @out.comment "Iterator for names and instances"
+      @out.comment " yields references matching reference and properties"
+      @out.comment
+      @out.def "each", "reference", "properties = nil", "want_instance = false"
+      @out.puts "result = Cmpi::CMPIObjectPath.new reference"
+      @out.puts
+      @out.comment "Set key properties"
+      @out.puts
+      properties :keys do |prop|
+	@out.puts(property_setter_line prop)
       end
-      out.puts "yield result unless want_instance"
-      out.puts
-      out.comment "Set non-key properties"
-      out.puts
-      properties(c, :nokeys) do |prop|
-	# using out.comment would break the line at col 72
-	out.puts "# #{property_setter_line prop}"
+      @out.puts "yield result unless want_instance"
+      @out.puts
+      @out.comment "Set non-key properties"
+      @out.puts
+      properties :nokeys do |prop|
+	# using @out.comment would break the line at col 72
+	@out.puts "# #{property_setter_line prop}"
       end
-      out.puts "yield result"
-      out.end
-      out.puts "public"
+      @out.puts "yield result"
+      @out.end
+      @out.puts "public"
     end
     #
     # Generate Class#initialize
     #
-    def mknew c,out
-      out.comment
-      out.comment "Provider initialization"
-      out.comment
-      out.def "initialize", "name", "broker", "context"
-      out.puts "@trace_file = STDERR"
-      out.puts "super name, broker, context"
-      out.end
+    def mknew
+      @out.comment
+      @out.comment "Provider initialization"
+      @out.comment
+      @out.def "initialize", "name", "broker", "context"
+      @out.puts "@trace_file = STDERR"
+      @out.puts "super name, broker, context"
+      @out.end
     end
 
     #
     # Generate create_instance
     #
-    def mkcreate c, out
-      out.def "create_instance", "context", "result", "reference", "newinst"
-      out.puts "#{LOG} \"create_instance ref \#{reference}, newinst \#{newinst.inspect}\""
-      out.puts "#{c.name}.new reference, newinst"
-      out.puts "result.return_objectpath reference"
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkcreate
+      @out.def "create_instance", "context", "result", "reference", "newinst"
+      @out.puts "#{LOG} \"create_instance ref \#{reference}, newinst \#{newinst.inspect}\""
+      @out.puts "#{@klass.name}.new reference, newinst"
+      @out.puts "result.return_objectpath reference"
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate enum_instance_names
     #
-    def mkenum_instance_names c, out
-      out.def "enum_instance_names", "context", "result", "reference"
-      out.puts "#{LOG} \"enum_instance_names ref \#{reference}\""
-      out.puts("each(reference) do |ref|").inc
-      out.puts "#{LOG} \"ref \#{ref}\""
-      out.puts "result.return_objectpath ref"
-      out.end
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkenum_instance_names
+      @out.def "enum_instance_names", "context", "result", "reference"
+      @out.puts "#{LOG} \"enum_instance_names ref \#{reference}\""
+      @out.puts("each(reference) do |ref|").inc
+      @out.puts "#{LOG} \"ref \#{ref}\""
+      @out.puts "result.return_objectpath ref"
+      @out.end
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate enum_instances
     #
-    def mkenum_instances c, out
-      out.def "enum_instances", "context", "result", "reference", "properties"
-      out.puts "#{LOG} \"enum_instances ref \#{reference}, props \#{properties.inspect}\""
-      out.puts("each(reference, properties, true) do |ref|").inc
-      out.puts "#{LOG} \"ref \#{ref}\""
-      out.puts "instance = CMPIInstance.new ref"
-      out.puts "result.return_instance instance"
-      out.end
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkenum_instances
+      @out.def "enum_instances", "context", "result", "reference", "properties"
+      @out.puts "#{LOG} \"enum_instances ref \#{reference}, props \#{properties.inspect}\""
+      @out.puts("each(reference, properties, true) do |ref|").inc
+      @out.puts "#{LOG} \"ref \#{ref}\""
+      @out.puts "instance = CMPIInstance.new ref"
+      @out.puts "result.return_instance instance"
+      @out.end
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate get_instance
     #
-    def mkget_instance c, out
-      out.def "get_instance", "context", "result", "reference", "properties"
-      out.puts "#{LOG} \"get_instance ref \#{reference}, props \#{properties.inspect}\""
-      out.puts("each(reference, properties, true) do |ref|").inc
-      out.puts "#{LOG} \"ref \#{ref}\""
-      out.puts "instance = CMPIInstance.new ref"
-      out.puts "result.return_instance instance"
-      out.puts "break # only return first instance"
-      out.end
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkget_instance
+      @out.def "get_instance", "context", "result", "reference", "properties"
+      @out.puts "#{LOG} \"get_instance ref \#{reference}, props \#{properties.inspect}\""
+      @out.puts("each(reference, properties, true) do |ref|").inc
+      @out.puts "#{LOG} \"ref \#{ref}\""
+      @out.puts "instance = CMPIInstance.new ref"
+      @out.puts "result.return_instance instance"
+      @out.puts "break # only return first instance"
+      @out.end
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate set_instance
     #
-    def mkset_instance c, out
-      out.def "set_instance", "context", "result", "reference", "newinst", "properties"
-      out.puts "#{LOG} \"set_instance ref \#{reference}, newinst \#{newinst.inspect}, props \#{properties.inspect}\""
-      out.puts("properties.each do |prop|").inc
-      out.puts "newinst.send \"\#{prop.name}=\".to_sym, FIXME"
-      out.end
-      out.puts "result.return_instance newinst"
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkset_instance
+      @out.def "set_instance", "context", "result", "reference", "newinst", "properties"
+      @out.puts "#{LOG} \"set_instance ref \#{reference}, newinst \#{newinst.inspect}, props \#{properties.inspect}\""
+      @out.puts("properties.each do |prop|").inc
+      @out.puts "newinst.send \"\#{prop.name}=\".to_sym, FIXME"
+      @out.end
+      @out.puts "result.return_instance newinst"
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate delete_instance
     #
-    def mkdelete_instance c, out
-      out.def "delete_instance", "context", "result", "reference"
-      out.puts "#{LOG} \"delete_instance ref \#{reference}\""
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkdelete_instance
+      @out.def "delete_instance", "context", "result", "reference"
+      @out.puts "#{LOG} \"delete_instance ref \#{reference}\""
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate exec_query
     #
-    def mkquery c, out
-      out.comment "query : String"
-      out.comment "lang : String"
-      out.def "exec_query", "context", "result", "reference", "query", "lang"
-      out.puts "#{LOG} \"exec_query ref \#{reference}, query \#{query}, lang \#{lang}\""
-      out.puts "result.done"
-      out.puts "true"
-      out.end
+    def mkquery
+      @out.comment "query : String"
+      @out.comment "lang : String"
+      @out.def "exec_query", "context", "result", "reference", "query", "lang"
+      @out.puts "#{LOG} \"exec_query ref \#{reference}, query \#{query}, lang \#{lang}\""
+      @out.puts "result.done"
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate cleanup
     #
-    def mkcleanup c, out
-      out.def "cleanup", "context", "terminating"
-      out.puts "#{LOG} \"cleanup terminating? \#{terminating}\""
-      out.puts "true"
-      out.end
+    def mkcleanup
+      @out.def "cleanup", "context", "terminating"
+      @out.puts "#{LOG} \"cleanup terminating? \#{terminating}\""
+      @out.puts "true"
+      @out.end
     end
 
     #
     # Generate valuemap classes
     #
-    def mkvaluemaps c, out
-      properties(c,:all) do |property|
+    def mkvaluemaps
+      properties :all do |property|
 	# get the Values and ValueMap qualifiers
 	values = property.Values
 	next unless values
 	valuemap = property.ValueMap
-	out.puts
-	out.puts("class #{property.name}").inc
-	out.puts("MAP = {").inc
+	@out.puts
+	@out.puts("class #{property.name}").inc
+	@out.puts("MAP = {").inc
 	# get to the array
 	values = values.value
 	valuemap = valuemap.value
@@ -238,13 +239,13 @@ module Genprovider
 	    raise "#{property.name}: Values #{val}, ValueMap empty"
 	  end
 	  if map =~ /\.\./
-	    out.comment "#{val.inspect} => #{map},"
+	    @out.comment "#{val.inspect} => #{map},"
 	  else
-	    out.puts "#{val.inspect} => #{map},"
+	    @out.puts "#{val.inspect} => #{map},"
 	  end
 	end
-	out.dec.puts "}"
-	out.end
+	@out.dec.puts "}"
+	@out.end
       end
     end
 
@@ -255,63 +256,66 @@ module Genprovider
     #
 
     def initialize c, name, out
+      @klass = c
+      @out = out
+
       #
       # Header: class name, provider name (Class qualifier 'provider')
       #
 
-      out.comment
-      out.comment "Provider #{name} for class #{c.name}"
-      out.comment
+      @out.comment
+      @out.comment "Provider #{name} for class #{@klass.name}"
+      @out.comment
 
-      out.puts("require 'syslog'").puts
-      out.puts("require 'cmpi/provider'").puts
-      out.puts("module Cmpi").inc
+      @out.puts("require 'syslog'").puts
+      @out.puts("require 'cmpi/provider'").puts
+      @out.puts("module Cmpi").inc
 
-      Genprovider::Class.mkdescription out, c
+      Genprovider::Class.mkdescription @out, @klass
       #
       # baseclass and interfaces
       #
       providertypes = []
-      providertypes << "InstanceProvider" if c.instance?
-      providertypes << "MethodProvider" if c.method?
-      providertypes << "AssociationProvider" if c.association?
-      providertypes << "IndicationProvider" if c.indication?
+      providertypes << "InstanceProvider" if @klass.instance?
+      providertypes << "MethodProvider" if @klass.method?
+      providertypes << "AssociationProvider" if @klass.association?
+      providertypes << "IndicationProvider" if @klass.indication?
 
       if providertypes.empty?
-	STDERR.puts "Assuming that #{c.name} defines an Instance"
+	STDERR.puts "Assuming that #{@klass.name} defines an Instance"
 	providertypes << "InstanceProvider"
       end
 
-      out.puts("class #{name} < #{providertypes.shift}").inc
-      out.puts
+      @out.puts("class #{name} < #{providertypes.shift}").inc
+      @out.puts
       providertypes.each do |t|
-	out.puts "include #{t}IF"
+	@out.puts "include #{t}IF"
       end
-      mkeach c,out
-      out.puts
-      mknew c,out
-      out.puts
-      if c.instance? || providertypes.empty?
-	mkcreate c, out
-	out.puts
-	mkenum_instance_names c, out
-	out.puts
-	mkenum_instances c, out
-	out.puts
-	mkget_instance c, out
-	out.puts
-	mkset_instance c, out
-	out.puts
-	mkdelete_instance c, out
-	out.puts
-	mkquery c, out
-	out.puts
-	mkcleanup c, out
-	out.puts
-	mkvaluemaps c, out
+      mkeach
+      @out.puts
+      mknew
+      @out.puts
+      if @klass.instance? || providertypes.empty?
+	mkcreate
+	@out.puts
+	mkenum_instance_names
+	@out.puts
+	mkenum_instances
+	@out.puts
+	mkget_instance
+	@out.puts
+	mkset_instance
+	@out.puts
+	mkdelete_instance
+	@out.puts
+	mkquery
+	@out.puts
+	mkcleanup
+	@out.puts
+	mkvaluemaps
       end
-      out.end # class
-      out.end # module
+      @out.end # class
+      @out.end # module
     end
   end
 end
